716ce7095eba1a92febd844b5fc058af1fc176df
AAZ <amelias.azzi@gmail.com>
2024-10-24 11:20:00 +0200

    👽️ Add send email route

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,19 @@
+import { sendEmail } from "@/utils/mail"
+
+export async function POST(request: Request) {
+    const { sender, recipient, message } = await request.json();
+    try {
+        const result = await sendEmail({
+            sender: sender,
+            receipient: recipient,
+            subject: 'Get in touch',
+            message: message
+        })
+
+        return Response.json({
+            accepted: result.accepted
+        })
+    } catch (e) {
+        return Response.json({ message: `Error to send email: ${e}`}, {status: 500})
+    }
+}
